5f17e71a409e624c7980edb9562ff6c5ae6ee0fe
jordan <jharrison1@qa.com>
2021-05-05 12:58:28 +0100

    removed nonsense

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import Container from './lifting-state/Container';
 import TrainerContainer from './trainers/TrainerContainer';
 
 function App() {
-    
-  const [val, setVal] = useState("bloop");
-
-  const handleChange = e => {
-    setVal(bloop => {
-      console.log(bloop);
-      return bloop + e.target.value;
-    });
-  }
 
   return (
     <div className="App">
       <TrainerContainer/>
       {/* <Container/> */}
-      <p>VAl: {val}</p>
-      <input onChange={handleChange}/>
     </div>
   );
 }
